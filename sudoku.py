c0bc80de25dd620eb1cba0a5409bd54c852d4ce6
Kram-Spr <77450241+Kram-Spr@users.noreply.github.com>
2021-01-21 14:18:45 -0500

    combined two functions into one

--- sudoku.py
+++ sudoku.py
@@ -33,9 +33,18 @@ class Grid:
             pygame.draw.line(win,(0,0,0), (0,i * space), (self.width,i*space), thickness)
             if i % 9 == 0:
                 pygame.draw.line(win,(0,0,0), (0,i * space), (self.width,i*space), 10)
+        
+        for i,r in enumerate(self.board, start=0):
+            for o,c in enumerate(r,start=0):
+                print(r[i])
+                print(c)
+                current_x = int(r[o] * 60)
+                current_y = int(i * 60)
+                current_rect = pygame.Rect((current_x, current_y),(60,60))
+                pygame.draw.rect(win, (random.randint(0,255),255,255), current_rect)
     
-    def create_rect_inputs(self, win):
-        pass
+    #def create_rect_inputs(self, win):
+        #pass
     
 def solver_activate(self):
     pass
@@ -44,7 +53,7 @@ def refresher(win):
     win.fill((255,255,255))
     full_board = Grid(9,9,600,600)
     full_board.divide_board(win)
-    full_board.create_rect_inputs(win)
+    #full_board.create_rect_inputs(win)
     pygame.display.update()
 
 def main():
@@ -59,7 +68,7 @@ def main():
 
         pygame.time.Clock().tick(40)
         mouse_pos = pygame.mouse.get_pos()
-        print(mouse_pos)
+        #print(mouse_pos)
         refresher(win)
 
 main()
