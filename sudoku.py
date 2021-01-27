4aa1cc46762168040f33642e11961d1c9386ef36
Kram-Spr <77450241+Kram-Spr@users.noreply.github.com>
2021-01-27 16:22:07 -0500

    Added comments

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,11 @@
+#Module imports (Pygame) to use for UI
 import pygame
 import os, sys
 import random
 import time
 
 class Grid:
+    #This is what the program reads from. Change this to change the starting board. (9x9 grid)
     board = [
         [7,8,0,4,0,0,1,2,0],
         [6,0,0,0,7,5,0,0,9],
@@ -15,7 +17,8 @@ class Grid:
         [1,2,0,0,0,7,4,0,0],
         [0,4,9,2,0,6,0,0,7]
     ]
- 
+    
+    #Runs when the class is called on. Just assigns variables.
     def __init__(self, row, col, width, height):
         self.row = row
         self.col = col
@@ -24,10 +27,13 @@ class Grid:
         self.current_x = 5
         self.current_y = 5
         self.grids = []
-        
+    
+    #This function draws the lines and rectangles seen on screen, it runs every frame when it is called.
     def divide_board(self, win):
+        #Initializes the font section of pygame to write text later
         pygame.font.init()
 
+        #The logic and math that goes towards drawing the lines of the board
         space = self.width / 9
         for i in range(self.row + 1):
             if i % 3 == 0 and i != 0:
@@ -39,6 +45,7 @@ class Grid:
             if i % 9 == 0:
                 pygame.draw.line(win,(0,0,0), (0,i * space), (self.width,i*space), 10)
         
+        #The logic and math that goes towards drawing the rectangles of the board
         for i,r in enumerate(self.board, start=0):
             for o,c in enumerate(r,start=0):
                 current_rect = pygame.Rect((self.current_x, self.current_y),(60,60))
@@ -58,23 +65,28 @@ class Grid:
                         self.current_x += 65
                         self.current_y += 0
 
+                #Draws the corresponding number from the board to the current rectangle in the correponsing row/column
                 current_font = pygame.font.SysFont('Arial', 30)
                 if str(c) == '0':
                     c = ''
                 current_text = current_font.render(str(c), True, (0,0,0))
                 win.blit(current_text, (grid.x + 20, grid.y + 10))
 
+    #This function is called on by the algorithm to determine if a number is valid in the certain spot in question
     def valid_input(self, input, rownum, colnum):
         valid = True
 
+        #Checks the current row
         for rn in self.board[rownum]:
             if rn == input:
                 valid = False
         
+        #Checks the current column
         for r,rv in enumerate(self.board):
             if rv[colnum] == input:
                 valid = False
         
+        #Checks the current 3x3 grid
         for row in range(0, 9, 3):
             for col in range(0,9,3):
                 temp = []
@@ -87,12 +99,14 @@ class Grid:
 
         return valid
 
+    #Finds and returns the nearest empty spot on the board. If there are no empty spots, the board is complete.
     def find_empty(self):
         for i,r in enumerate(self.board, start=0):
             for o,c in enumerate(r, start=0):
                 if self.board[i][o] == 0:
                     return (i,o)
 
+    #A function to run through numbers 1-9 and assign them to the board if its valid. Also backtracks.
     def solver_activate(self, win):
         found = self.find_empty()
         if not found:
@@ -116,6 +130,7 @@ class Grid:
 
 solve_button = ''
 
+#The actual function that updates the screen correctly every frame
 def refresher(win):
     global solve_button
     global full_board
@@ -130,6 +145,7 @@ def refresher(win):
     win.blit(solve_text, (solve_button.x + 50, solve_button.y + 15))
     pygame.display.update()
 
+#Runs on start. Gives properties to the window and checks for events such as clicking the exit button.
 def main():
     global solve_button
     debounce = False
